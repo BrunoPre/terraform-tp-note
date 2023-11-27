48cee57a5305186ccecf502a724b2f41f8f311d0
Bruno PREKA <brunopre44@gmail.com>
2023-11-27 16:01:57 +0100

    edit output from dockerized terraform

--- docker/main.tf
+++ docker/main.tf
@@ -11,7 +11,12 @@ terraform {
   }
 }
 
-output "app_endpoint" {
+output "vote_endpoint" {
   value       = "http://localhost:${docker_container.vote.ports[0].external}"
-  description = "The URL endpoint to access the Guestbook application"
+  description = "The URL endpoint to access the vote application"
+}
+
+output "result_endpoint" {
+  value       = "http://localhost:${docker_container.result.ports[0].external}"
+  description = "The URL endpoint to access the result application"
 }

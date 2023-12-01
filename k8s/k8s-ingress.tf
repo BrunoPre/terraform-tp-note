017562c1a5aa7b72016e119f83f29e1a31e280d6
Bruno PREKA <brunopre44@gmail.com>
2023-12-01 12:42:50 +0100

    silly forgotten comment add/removal

--- k8s/k8s-ingress.tf
+++ k8s/k8s-ingress.tf
@@ -2,8 +2,6 @@ module "k8s-ingress-frontend-vote" {
   source = "./modules/k8s-ingress"
 
   metadata_name = "frontend-vote-ingress"
-  # service_name = "vote"
-  # service_port = 5000
   service_name = module.k8s-service-vote.svc_name
   service_port = module.k8s-service-vote.svc_port
 }
@@ -12,8 +10,6 @@ module "k8s-ingress-frontend-result" {
   source = "./modules/k8s-ingress"
 
   metadata_name = "frontend-result-ingress"
-  # service_name = "result"
   service_name = module.k8s-service-result.svc_name
-  # service_port = 5001
   service_port = module.k8s-service-result.svc_port
 }
